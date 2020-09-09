3ccda1e9e19b4f1d29a65bcbb562ce43a47008bd
1234siddsingh <1234siddsingh@gmail.com>
2020-09-09 21:54:05 +0530

    Changing UI of the page

--- src/components/SearchGuide.js
+++ src/components/SearchGuide.js
@@ -4,6 +4,7 @@ const SearchGuide = () => {
   return (
     <div className="card">
       <div className="card-body">
+        <p id="wearmask"><img src="WearMask.ico"></img> Wear Mask Guys </p>
         <h1>Welcome to Page - Siddharth Kumar</h1>
         <p>Type a username in text box to see user Profile</p>
       </div>
