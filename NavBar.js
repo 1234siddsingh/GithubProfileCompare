3875d4a148ce54d4f7ac5f99a11cfce0188ceb46
1234siddsingh <1234siddsingh@gmail.com>
2020-09-09 20:45:53 +0530

    adding change for navigation bar and help content

---
+++ NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ""
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      username: event.target.value
+    }, () => {
+      if (this.state.username === "") {
+        this.handleSubmit()
+      }
+    });
+  };
+  handleSubmit = () => {
+    this.props.fetchUserDetails(this.state.username);
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-primary" >
+        <span className="navbar-brand mb-0 h1">GithubCompare</span>
+        <form className="form-inline" onSubmit={(e) => e.preventDefault()}>
+          <input className="form-control mr-sm-2" type="text" placeholder="Search by username" value={this.state.username} onChange={this.handleChange} />
+          <button className="btn btn-light btn-outline-light my-2 my-sm-0" type="submit" onClick={this.handleSubmit}>Compare</button>
+        </form>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
---
+++ SearchGuide.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchGuide = () => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h1>Welcome to Page - Siddharth Kumar</h1>
+        <p>Type a username in text box to see user Profile</p>
+      </div>
+    </div>
+  );
+}
+
+export default SearchGuide;
+
+
+
